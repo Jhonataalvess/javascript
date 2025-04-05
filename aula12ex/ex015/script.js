3ec73e6f94c334c07f58c5d8d8755ab61c390803
Jhonata100 <Jhonatagg100@gmail.com>
2025-04-05 11:47:00 -0300

    finalizei os exercícios da aula 14

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -8,6 +8,44 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/foto-bebe-m.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src','imagens/foto-jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src','imagens/foto-adulto-m.jpg')
+            } else {
+                // Idoso 
+                img.setAttribute('src','imagens/foto-idoso-m.jpg')
+            }
+                
+        } else {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/foto-bebe-f.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src','imagens/foto-jovem-f.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src','imagens/foto-adulto-f.jpg')
+            } else {
+                // Idoso 
+                img.setAttribute('src','imagens/foto-idoso-f.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos.</p>`
+        res.appendChild(img)
+        img.style.borderRadius = '50%'
     }
 }
